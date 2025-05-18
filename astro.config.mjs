696fa0394fe5d77fa4ebacaaa49ba37b7875bbb6
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2025-05-18 15:25:56 -0700

    potentially fix vite issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,20 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    worker: {
+      // Attempt to force the output format of web workers to ES modules
+      format: 'es',
+      rollupOptions: {
+        output: {
+          // You can also try explicitly setting the format for worker chunks here
+          format: 'es'
+        }
+      }
+    },
+    optimizeDeps: {
+      exclude: [
+        'cubing']
+    }
   }
 });
